358dba039c225f89c2cf766a1bf4b049b0df2daa
Eddy <155239294+Eddy3129@users.noreply.github.com>
2025-05-29 14:18:45 +0800

    ft_itoa

--- Makefile
+++ Makefile
@@ -25,7 +25,8 @@ SRCS = 	ft_atoi.c \
 		ft_strrchr.c \
 		ft_strncmp.c \
 		ft_strtrim.c \
-		ft_split.c
+		ft_split.c \
+		ft_itoa.c
 
 # Object files
 OBJS = $(SRCS:.c=.o)
--- main.c
+++ main.c
@@ -1,23 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "libft.h"
+#include <stdio.h>
 
 int main(void)
 {
-    char *str = "Hello, World! maka baka da mn you";
-    char **words = ft_split(str, ' ');
-
-    if (words == NULL)
-    {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
-
-    int i = 0;
-    while (words[i] != NULL)
-    {
-        printf("%s\n", words[i]);
-        free(words[i]);
-        i++;
-    }
+	printf("%s\n", ft_itoa(123456789));
+    printf("%s\n", ft_itoa(-2147483648));
+    printf("%s\n", ft_itoa(0));
+    printf("%s\n", ft_itoa(100000000));
+    printf("%s\n", ft_itoa(101030010));
 }
