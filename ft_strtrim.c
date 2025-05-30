30d312cc2f8ad1965dd258dfe354278f5ef29086
Eddy <155239294+Eddy3129@users.noreply.github.com>
2025-05-30 18:42:26 +0800

    fix all errors

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 12:46:17 by eddlim            #+#    #+#             */
-/*   Updated: 2025/05/26 22:09:24 by marvin           ###   ########.fr       */
+/*   Updated: 2025/05/30 18:41:34 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,41 +23,50 @@ int	is_trim(const char *set, char c)
 	return (0);
 }
 
+size_t	find_start(const char *s1, const char *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && is_trim(set, s1[i]))
+		i++;
+	return (i);
+}
+
+size_t	find_end(const char *s1, const char *set)
+{
+	size_t	j;
+
+	j = 0;
+	while (s1[j])
+		j++;
+	if (j == 0)
+		return (0);
+	j--;
+	while (s1[j] && is_trim(set, s1[j]))
+		j--;
+	return (j);
+}
+
 char	*ft_strtrim(const char *s1, const char *set)
 {
 	size_t	start;
 	size_t	end;
 	size_t	len;
-	size_t	idx;
 	char	*dest;
 
-	idx = 0;
 	if (!s1)
 		return (NULL);
-	start = 0;
-	while (s1[start] && is_trim(set, s1[start]))
-		start++;
-	end = ft_strlen(s1);
-	while (end > start && is_trim(set, s1[end - 1]))
-		end--;
-	len = end - start;
-	dest = ft_calloc(len + 1, sizeof(char));
+	if (!set)
+		return (ft_strdup(s1));
+	start = find_start(s1, set);
+	end = find_end(s1, set);
+	if (s1[start] == '\0')
+		return (ft_strdup(""));
+	len = end - start + 1;
+	dest = (char *)ft_calloc((len + 1), sizeof(char));
 	if (!dest)
 		return (NULL);
-	while (idx < len)
-	{
-		dest[idx] = s1[start + idx];
-		idx++;
-	}
+	ft_memmove(dest, &s1[start], len);
 	return (dest);
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*s1 = "ahahelloahaha";
-	char	*s2 = "ah";
-	printf("%s\n", ft_strtrim(s1, s2));
-}
-*/
